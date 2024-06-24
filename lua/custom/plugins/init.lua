6ac66353db1f194470c972f5adcece34acb74140
Tensor <narutokelechi@gmail.com>
2024-06-24 15:51:25 +0100

    my neovim config v1

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,35 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ 'wakatime/vim-wakatime', lazy = false },
+	{
+  	"Exafunction/codeium.nvim",
+  	cmd = "Codeium",
+ 	 build = ":Codeium Auth",
+ 	 opts = {},
+	},
+	{
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+}
